ed67d858cf8b6172e3ed257d29448880701bc025
Neftali Yagua <neftali.yagua@cortex-ia.com.co>
2025-12-15 14:43:39 -0500

    fix: corregir rutas de relay-ejemplo-hotel a hotel

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/relay-ejemplo-hotel/',
+  base: '/hotel/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
